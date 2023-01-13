1e7d61cdfd0051b8a46b7b7e34f2721ec575b3ec
Louis Vignier <louis.vignier@jetbrains.com>
2023-01-13 18:16:17 +0100

    Update report

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-// Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press ⌥⏎ with your caret at the highlighted text to see how
