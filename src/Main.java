2251940af11c6f01eda1a619879d2b14c70b0937
Louis Vignier <louis.vignier@jetbrains.com>
2023-01-13 18:18:50 +0100

    Update report

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,20 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         // Press ⌃R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 31; i++) {
 
             // Press ⌃D to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing ⌘F8.
             System.out.println("i = " + i);
         }
 
-        Bar.foo();
+        Bar.bar();
     }
 }
 
 class Bar {
     @Deprecated(forRemoval=true)
-    public static void foo() {
+    public static void bar() {
 
     }
 }
